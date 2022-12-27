346b5060b020543461e18cde8bb9caccd7a7a12f
amorimcode <bruno.amorim032@gmail.com>
2022-12-27 11:03:55 -0300

    update markdown

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,17 +2,91 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   name: 'default',
-  title: 'sanity-blog',
-
-  projectId: 'pw60u7vf',
+  title: 'leerob.io',
+  projectId: '56a8ffnw',
   dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
+  plugins: [deskTool(), markdownSchema()],
   schema: {
-    types: schemaTypes,
+    types: [
+      {
+        name: 'post',
+        type: 'document',
+        title: 'Post',
+        fields: [
+          {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+          },
+          {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+            },
+          },
+          {
+            name: 'content',
+            title: 'Content',
+            type: 'markdown',
+          },
+          {
+            name: 'excerpt',
+            title: 'Excerpt',
+            type: 'string',
+          },
+          {
+            name: 'coverImage',
+            title: 'Cover Image',
+            type: 'image',
+          },
+          {
+            name: 'date',
+            title: 'Date',
+            type: 'datetime',
+          },
+        ],
+      },
+      {
+        name: 'snippet',
+        type: 'document',
+        title: 'Snippet',
+        fields: [
+          {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+          },
+          {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+            },
+          },
+          {
+            name: 'content',
+            title: 'Content',
+            type: 'markdown',
+          },
+          {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+          },
+          {
+            name: 'logo',
+            title: 'Logo',
+            type: 'image',
+          },
+        ],
+      },
+    ],
   },
 })
